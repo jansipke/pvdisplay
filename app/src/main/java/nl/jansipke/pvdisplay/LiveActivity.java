e2a54aa9edbf122e2ddffbb7ae1179f65b8b6114
Jan Sipke van der Veen <j.s.vanderveen@mgrid.net>
2016-08-21 17:36:19 +0200

    WIP

--- app/src/main/java/nl/jansipke/pvdisplay/LiveActivity.java
+++ app/src/main/java/nl/jansipke/pvdisplay/LiveActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -41,8 +40,8 @@ public class LiveActivity extends AppCompatActivity {
     private final NumberFormat powerFormat = new DecimalFormat("#0");
     private final NumberFormat energyFormat = new DecimalFormat("#0.000");
 
-    private int ago = 0;
-    private LineGraphSeries<DataPoint> powerSeries;
+    private int ago = 26;
+    private LineGraphSeries<DataPoint> powerSeries; // TODO Fixed size (24 hours divided by 5 minutes)
     private LineGraphSeries<DataPoint> energySeries;
 
     @Override
@@ -77,7 +76,7 @@ public class LiveActivity extends AppCompatActivity {
     private void initGraph() {
         GraphView graph = (GraphView) findViewById(R.id.graph);
 
-        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getApplicationContext(), timeFormat));
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getApplicationContext(), dateTimeFormat));
         graph.getGridLabelRenderer().setNumHorizontalLabels(3);
         graph.getGridLabelRenderer().setNumVerticalLabels(4);
         graph.getGridLabelRenderer().setHumanRounding(false);
@@ -86,25 +85,26 @@ public class LiveActivity extends AppCompatActivity {
         graph.getViewport().setMinY(0);
         graph.getViewport().setMaxY(1500); // TODO Replace with historical maximum
         graph.getSecondScale().setMinY(0);
-        graph.getSecondScale().setMaxY(12000); // TODO Replace with historical maximum
+        graph.getSecondScale().setMaxY(12); // TODO Replace with historical maximum
 
         graph.getLegendRenderer().setVisible(true);
         graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
 
         powerSeries = new LineGraphSeries<>();
         powerSeries.setColor(Color.BLUE);
-        powerSeries.setTitle("Power (W)");
+        powerSeries.setTitle(getResources().getString(R.string.graph_legend_power));
         graph.addSeries(powerSeries);
 
         energySeries = new LineGraphSeries<>();
         energySeries.setColor(Color.RED);
-        energySeries.setTitle("Energy (Wh)");
+        energySeries.setTitle(getResources().getString(R.string.graph_legend_energy));
         graph.getSecondScale().addSeries(energySeries);
     }
 
     private DataPoint createDataPoint(String date, String time, double value) {
         try {
             Date dateTime = dateTimeFormat.parse(date + " " + time);
+            Log.i(TAG, date + " " + time + " " + value);
             return new DataPoint(dateTime, value);
         } catch (ParseException e) {
             Log.e(TAG, "Error parsing date time", e);
@@ -124,15 +124,18 @@ public class LiveActivity extends AppCompatActivity {
             energyDataPoints[0] = createDataPoint(date, "00:00", 0.0);
             energyDataPoints[1] = createDataPoint(date, "23:59", 0.0);
         } else {
-            powerDataPoints = new DataPoint[livePvData.size()];
-            energyDataPoints = new DataPoint[livePvData.size()];
-            for (int i = 0; i < livePvData.size(); i++) {
+            int size = 10; // livePvData.size()
+            powerDataPoints = new DataPoint[size];
+            energyDataPoints = new DataPoint[size];
+            for (int i = 0; i < size; i++) {
                 LivePvDatum livePvDatum = livePvData.get(i);
+                Log.i(TAG, livePvDatum.toString());
                 powerDataPoints[i] = createDataPoint(livePvDatum.getDate(), livePvDatum.getTime(), livePvDatum.getPowerGeneration());
-                energyDataPoints[i] = createDataPoint(livePvDatum.getDate(), livePvDatum.getTime(), livePvDatum.getEnergyGeneration());
+                energyDataPoints[i] = createDataPoint(livePvDatum.getDate(), livePvDatum.getTime(), livePvDatum.getEnergyGeneration() / 1000.0);
             }
         }
 
+        Log.i(TAG, "" + livePvData.size() + ", " + powerDataPoints.length + ", " + energyDataPoints.length);
         powerSeries.resetData(powerDataPoints);
         energySeries.resetData(energyDataPoints);
     }
@@ -147,7 +150,7 @@ public class LiveActivity extends AppCompatActivity {
             View row = getLayoutInflater().inflate(R.layout.table_row, null);
             ((TextView) row.findViewById(R.id.content1)).setText(livePvDatum.getTime());
             ((TextView) row.findViewById(R.id.content2)).setText(powerFormat.format(livePvDatum.getPowerGeneration()));
-            ((TextView) row.findViewById(R.id.content3)).setText(energyFormat.format(livePvDatum.getEnergyGeneration() / 1000));
+            ((TextView) row.findViewById(R.id.content3)).setText(energyFormat.format(livePvDatum.getEnergyGeneration() / 1000.0));
             linearLayout.addView(row);
         }
     }
@@ -165,10 +168,11 @@ public class LiveActivity extends AppCompatActivity {
         String year = date.substring(0, 4);
         String month = date.substring(4, 6);
         String day = date.substring(6, 8);
+        String title = "Live   " + year + "-" + month + "-" + day;
 
         ActionBar supportActionBar = getSupportActionBar();
         if (supportActionBar != null) {
-            supportActionBar.setTitle("Live   " + year + "-" + month + "-" + day);
+            supportActionBar.setTitle(title);
         }
 
         PvDataOperations pvDataOperations = new PvDataOperations(getApplicationContext());
--- app/src/main/java/nl/jansipke/pvdisplay/data/LivePvDatum.java
+++ app/src/main/java/nl/jansipke/pvdisplay/data/LivePvDatum.java
@@ -31,6 +31,6 @@ public class LivePvDatum {
     }
 
     public String toString() {
-        return date + " " + time + " " + energyGeneration + "Wh " + powerGeneration + " W";
+        return date + " " + time + " "  + powerGeneration + "W " + energyGeneration + "Wh";
     }
 }
